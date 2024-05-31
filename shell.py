bba9875893f1b1e509bd07ad9309c5aa7cecb4d3
Emma Wang <ejw38@calvin.edu>
2024-05-31 14:50:40 -0400

    actual fix \n

--- shell.py
+++ shell.py
@@ -55,7 +55,7 @@ class Parser:
     def parse(self, string):
     
         # handle newline
-        if string == "\n":
+        if string == "\n" or string == "":
             string = "nothing"
         
         parsed = string.split()
